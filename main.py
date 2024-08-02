16b0dedb72aeb12aeb9123f4cf91d055741dd20d
jayant khandebharad <jkhandebharad@cloudlex.com>
2024-08-02 11:19:49 +0530

    first commit

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from flask import Flask
-from openai import OpenAI
-import os
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return "Hello, world!"
-
-if __name__ == '__main__':
-    print("hello world")
-    
+print("hello world")
