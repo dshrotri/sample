f1097fafe51b5b5c30d1673ce721bc2805ca0d33
JAYANT SANTOSH  KHANDEBHARAD <60604715+Jayantkhandebharad@users.noreply.github.com>
2024-08-02 06:48:47 +0000

    printing value of sum

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import sys
 if __name__ == "__main__":
     input_text = sys.stdin.read().strip()
     # Example processing logic
-    print(input_text)  # Replace with actual logic
+    sum = 0
+    for i in range(10):
+        sum+=i
+    print(sum)  # Replace with actual logic
